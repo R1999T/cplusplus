80c51507517fa0b09d648fd51a65e7b16b2b0693
R1999T <raghavtiwari7151@gmail.com>
2020-12-11 01:28:03 +0530

    updated

---
+++ challenges/recursion/stringrecursion/prob11.cpp
@@ -0,0 +1,26 @@
+//count the number of possible paths in a maze without obstecales?
+
+#include<iostream>
+using namespace std;
+
+int countPathMaze(int n, int i, int j)
+{
+    if (i ==n-1 && j==n-1)
+    {
+        return 1;
+    }
+    if (i >=n || j>=n)
+    {
+        return 0;
+    }
+
+    return countPathMaze(n, i+1, j)  + countPathMaze(n, i,j+1);
+
+}
+
+int main()
+{
+    cout<< countPathMaze(4, 0, 0)<< endl;
+    return 0;
+}
+
